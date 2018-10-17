fcf9710ba50fcaa6217638bb7369758fb255b2a6
1wmcmillin <42749672+1wmcmillin@users.noreply.github.com>
2018-10-17 14:00:10 -0500

    Add files via upload
    second function

---
+++ loops.js
@@ -0,0 +1,33 @@
+// For-loop functions
+
+function positiveIntSum(/* Give it one input */){
+let sum = 0;
+let n = 100;
+for(let i = 1; i <= n; i++){
+    sum = sum + i;
+    return sum
+}
+
+
+}
+
+
+function additionPatternSum(/* Give it three inputs */){
+
+}
+
+
+
+
+// While-loop function
+
+function multiplicationPatternSum(/* Give it three inputs */){
+
+
+}
+
+
+
+
+
+//Later copy paste the checking code below here.
